26b8649456c0a8e825a1d059dac131a960baebcc
李志杰 <lizhijie04@meituan.com>
2025-04-14 18:05:11 +0800

    增加部署文件

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 如果要部署到GitHub Pages，请取消下面一行的注释并替换为您的仓库名
+  // base: '/games-demo/',
 }) 
